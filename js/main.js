b013c6024bb6e008ee00d07b39a6c53dab6ea963
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-05 17:18:22 +0100

    added one obstacle moving from right to left, centered

--- js/main.js
+++ js/main.js
@@ -10,19 +10,55 @@ class Player {
     moveUp(){
         if (this.positionY > 0) {
             this.positionY -= 70;                            //player is moving by block of 70px
-            this.playerElm.style.top = this.positionY + "px" 
+            this.playerElm.style.top = this.positionY + "px";
         }
     };
     
     moveDown(){
         if (this.positionY < 420) {
             this.positionY += 70;
-            this.playerElm.style.top = this.positionY + "px"
+            this.playerElm.style.top = this.positionY + "px";
         }
     };
 }
 
+class Obstacle {
+    constructor (){
+        this.width = 70;
+        this.height = 70;
+        this.positionX = 1000;
+        this.positionY = 245 - (this.height / 2);
+        this.obstacleElm = null;
+        this.createDomElement();
+    }
+
+    createDomElement(){
+        this.obstacleElm = document.createElement('div'); 
+
+        this.obstacleElm.className = "obstacle";
+        this.obstacleElm.style.width = this.width  + "px";
+        this.obstacleElm.style.height = this.height  + "px";
+
+
+        const boardElm = document.getElementById("game-board");
+        boardElm.appendChild(this.obstacleElm);
+    };
+
+    moveLeft(){
+        this.positionX = this.positionX - 15;
+        this.obstacleElm.style.left = this.positionX + "px";
+        this.obstacleElm.style.top = this.positionY + "px";
+    }
+}
+
 const myPlayer = new Player();
+const myObstacle = new Obstacle();
+
+
+setInterval(function(){
+    myObstacle.moveLeft();
+}, 1000);
+
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowUp"){
