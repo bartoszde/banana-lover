f4a829511ef07a32fd0f53c57a6bbbdede62761e
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-05 19:34:05 +0100

    detect colision obs-player,fixed player appears at the end,added Game class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,68 @@
 const board = document.getElementById("game-board")
 
+class Game {
+    constructor(){
+        this.player = null;
+        this.obstaclesArr = [];  //will store instances of the class Obstacle
+    }
+    start(){
+        this.player = new Player();
+        this.attachEventListeners();
+
+
+        //create obstacles
+        setInterval( () => {  
+            const myObstacle = new Obstacle();
+            this.obstaclesArr.push(myObstacle);
+        }, 4000)
+        
+        //move all obstacles
+        setInterval( () => {   
+            this.obstaclesArr.forEach( (obstacleInstance) => {
+                obstacleInstance.moveLeft();
+                this.detectCollision(obstacleInstance); 
+            });
+        }, 200);
+    }
+    attachEventListeners(){
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "ArrowUp"){
+                this.player.moveUp();
+            } else if (event.key === "ArrowDown") {
+                this.player.moveDown();
+            }
+        });
+    }
+
+    detectCollision(obstacleInstance){
+        if (
+            this.player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            this.player.positionX + this.player.width > obstacleInstance.positionX &&
+            this.player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            this.player.height + this.player.positionY > obstacleInstance.positionY
+          ){
+            //console.log("game over my fren!");
+            window.location.href = "./gameover.html";
+          }
+    }
+}
+
 class Player {
     constructor (){
+        this.width = 70;
+        this.height = 70;
         this.positionX = 0;
-        this.positionY = 0;
+        this.positionY = 245 - (this.height / 2);
         this.playerElm = document.getElementById("player");
+        this.playerElm.style.bottom = this.positionY + "px";
+
+        this.playerElm.style.width = this.width + "px";
+        this.playerElm.style.height = this.height + "px";
     }
 
     moveUp(){
         if (this.positionY > 0) {
-            this.positionY -= 70;                            //player is moving by block of 70px
+            this.positionY -= 70;                  //player is moving by block of 70px
             this.playerElm.style.top = this.positionY + "px";
         }
     };
@@ -38,32 +91,23 @@ class Obstacle {
         this.obstacleElm.className = "obstacle";
         this.obstacleElm.style.width = this.width  + "px";
         this.obstacleElm.style.height = this.height  + "px";
-
+        this.obstacleElm.style.top = this.positionY + "px";
 
         const boardElm = document.getElementById("game-board");
         boardElm.appendChild(this.obstacleElm);
     };
 
     moveLeft(){
-        this.positionX = this.positionX - 15;
+        this.positionX = this.positionX - 10;
         this.obstacleElm.style.left = this.positionX + "px";
-        this.obstacleElm.style.top = this.positionY + "px";
+        
     }
 }
 
-const myPlayer = new Player();
-const myObstacle = new Obstacle();
+
+const game = new Game();
+game.start();
 
 
-setInterval(function(){
-    myObstacle.moveLeft();
-}, 1000);
 
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowUp"){
-        myPlayer.moveUp();
-    } else if (event.key === "ArrowDown") {
-        myPlayer.moveDown();
-    }
-});
